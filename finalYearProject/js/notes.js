ad17a241a5622b38013920239ab53f15b9f52786
Matthew Care <matty_care@hotmai.co.uk>
2013-10-09 22:31:02 +0100

    Big changes to the detector
    Added real time detection
    Added display
    Few other things

--- finalYearProject/js/notes.js
+++ finalYearProject/js/notes.js
@@ -1,54 +1,9 @@
-'use-strict';
-
 var note = {
 	CONSTANTFREQ: 440,
 	notes: ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'],
-	get: function (freq) {
+	get: function (freq, cb) {
 		if (!freq)
-			return 'please enter a number';
-		return this.notes[(69 + Math.round(12 * ( Math.log(freq / this.CONSTANTFREQ) ) / Math.log(2) )) % 12];
+			return cb('Enter something.');
+		return cb(null, this.notes[(69 + Math.round(12 * ( Math.log(freq / this.CONSTANTFREQ) ) / Math.log(2) )) % 12]);
 	}
 };
-
-var analyser = null;
-
-
-
-function convertToMono( input ) {
-    var splitter = AudioContext.createChannelSplitter(2),
-    	merger = AudioContext.createChannelMerger(2);
-
-    input.connect( splitter );
-    splitter.connect( merger, 0, 0 );
-    splitter.connect( merger, 0, 1 );
-    return merger;
-}
-
-function error() {
-    alert('Stream generation failed.');
-}
-
-function getUserMedia(dictionary, callback) {
-    try {
-        if (!navigator.getUserMedia)
-        	navigator.getUserMedia = navigator.webkitGetUserMedia;
-        navigator.getUserMedia(dictionary, callback, error);
-    } catch (e) {
-        alert('getUserMedia threw exception :' + e);
-    }
-}
-
-function gotStream(stream) {
-    // Create an AudioNode from the stream.
-    var mediaStreamSource = AudioContext.createMediaStreamSource(stream);
-
-    // Connect it to the destination.
-    analyser = AudioContext.createAnalyser();
-    analyser.fftSize = 2048;
-    convertToMono( mediaStreamSource ).connect( analyser );
-    updatePitch();
-}
-
-function toggleLiveInput() {
-    getUserMedia({audio:true}, gotStream);
-}
---
+++ finalYearProject/js/stream.js
@@ -0,0 +1,171 @@
+navigator.getMedia = (navigator.getUserMedia ||
+                      navigator.webkitGetUserMedia ||
+                      navigator.mozGetUserMedia ||
+                      navigator.msGetUserMedia);
+
+var stream = {
+
+    analyser: null,
+    audioContext: null,
+    localStream: null,
+    animationFrameId: null,
+    tracks: null,
+    buffer: null,
+    bufferLength: 1024,
+    MINVAL: 134,
+    noteStrings: ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"],
+    detectorElem: document.getElementById('detector'),
+    pitchElem: document.getElementById('pitch'),
+    noteElem: document.getElementById('note'),
+    detuneElem: document.getElementById('cents'),
+    detuneAmount: document.getElementById('centAmount'),
+
+    startMedia: function () {
+        this.audioContext = new webkitAudioContext();
+        this.buffer = new Uint8Array(this.bufferLength);
+        this.initUserMedia({audio: true}, this.gotStream.bind(this), this.errorCb());
+    },
+
+    initUserMedia: function (obj, cb, err) {
+        navigator.getMedia(obj, cb, err);
+    },
+
+    gotStream: function (localStream) {
+        this.localStream = localStream;
+        // Create an AudioNode from the localStream.
+        var mediaStreamSource = this.audioContext.createMediaStreamSource(localStream);
+        // Connect it to the destination.
+        this.analyser = this.audioContext.createAnalyser();
+        this.analyser.fftSize = 2048;
+        mediaStreamSource.connect(this.analyser);
+        this.updatePitch();
+    },
+
+    noteFromPitch: function (frequency) {
+        var noteNum = 12 * (Math.log(frequency / 440) / Math.log(2));
+        return Math.round(noteNum) + 69;
+    },
+
+    frequencyFromNoteNumber: function (note) {
+        return 440 * Math.pow(2, (note - 69) / 12);
+    },
+
+    centsOffFromPitch: function (frequency, note) {
+        return Math.floor(1200 * Math.log(frequency / this.frequencyFromNoteNumber(note)) / Math.log(2));
+    },
+
+    findNextPositiveZeroCrossing: function (start) {
+        var i = Math.ceil(start),
+            lastZero = -1,
+            t = 0;
+
+        while (i < this.bufferLength && (this.buffer[i] > 128)) {
+            i++;
+        }
+
+        if (i >= this.bufferLength) {
+            return -1;
+        }
+
+        while (i < this.bufferLength && ((t = this.buffer[i]) < this.MINVAL)) {
+            if (t >= 128) {
+                if (lastZero === -1) {
+                    lastZero = i;
+                }
+            } else {
+                lastZero = -1;
+            }
+            i++;
+        }
+
+        if (lastZero === -1) {
+            lastZero = i;
+        }
+
+        if (i === this.bufferLength) {
+            return -1;
+        }
+
+        if (lastZero === 0) {
+            return 0;
+        }
+
+        t = (128 - this.buffer[lastZero - 1]) / (this.buffer[lastZero] - this.buffer[lastZero - 1]);
+        return lastZero + t;
+    },
+
+    updatePitch: function () {
+        var cycles = [],
+            lastZero = this.findNextPositiveZeroCrossing(0),
+            nextZero = 0,
+            i = 0,
+            n = 0;
+
+        this.analyser.getByteTimeDomainData(this.buffer);
+
+        while (lastZero !== -1) {
+            nextZero = this.findNextPositiveZeroCrossing(lastZero + 1);
+            if (nextZero > -1) {
+                cycles.push(nextZero - lastZero);
+            }
+            lastZero = nextZero;
+
+            n++;
+            if (n > 1000) {
+                break;
+            }
+        }
+
+        var numberOfCycles = cycles.length,
+            sum = 0,
+            pitch = 0;
+
+        for (i = 0; i < numberOfCycles; i++) {
+            sum += cycles[i];
+        }
+
+        if (numberOfCycles) {
+            sum /= numberOfCycles;
+            pitch = this.audioContext.sampleRate / sum;
+        }
+
+        if (numberOfCycles === 0) {
+            this.pitchElem.innerText = "--";
+            this.noteElem.innerText = "-";
+            this.detuneElem.className = "";
+            this.detuneAmount.innerText = "--";
+
+        } else {
+            this.pitchElem.innerText = Math.floor(pitch);
+            var note =  this.noteFromPitch(pitch),
+                detune = this.centsOffFromPitch(pitch, note);
+            this.noteElem.innerText = this.noteStrings[note % 12];
+            if (detune === 0) {
+                this.detuneElem.className = "";
+                this.detuneAmount.innerText = "--";
+            } else {
+                if (detune < 0) {
+                    this.detuneElem.className = "flat";
+                } else {
+                    this.detuneElem.className = "sharp";
+                }
+                this.detuneAmount.innerText = Math.abs(detune);
+            }
+        }
+
+        if (!window.requestAnimationFrame) {
+            window.requestAnimationFrame = window.webkitRequestAnimationFrame;
+        }
+        this.animationFrameId = window.requestAnimationFrame(this.updatePitch.bind(this));
+    },
+
+    errorCb: function (err) {
+        if (err) {
+            console.log('Massive error');
+        }
+    },
+
+    stopMedia: function () {
+        console.log('No idea yet');
+    }
+};
