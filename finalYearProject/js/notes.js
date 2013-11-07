1a3d3cffb0219e87821a9adcf73d81b4404bc5a8
Matthew Care <matty_care@hotmai.co.uk>
2013-11-07 14:34:51 +0000

    removed final year project
    Put final year project in a repo of its own

--- finalYearProject/js/notes.js
+++
@@ -1,9 +0,0 @@
-var note = {
-	CONSTANTFREQ: 440,
-	notes: ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'],
-	get: function (freq, cb) {
-		if (!freq)
-			return cb('Enter something.');
-		return cb(null, this.notes[(69 + Math.round(12 * ( Math.log(freq / this.CONSTANTFREQ) ) / Math.log(2) )) % 12]);
-	}
-};
